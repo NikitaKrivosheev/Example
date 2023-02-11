84a513499dcd731954169a0d87e15002f11dde1f
Nikita Krivosheev <nikita47598@mail.ru>
2023-02-11 12:22:54 +0300

    ifelse

---
+++ Example005_ifelse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Привет, как тебя зовут ?");
+string userName = Console.ReadLine();
+string name = "маша";
+if (userName.ToLower() == name)
+{
+    Console.Write("Привет дорогая, " + name);
+} 
+else 
+{
+    Console.Write("Привет, " + userName);
+}
