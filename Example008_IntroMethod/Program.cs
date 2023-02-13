3855a01c4e362bc8e6a5bbf293485cf658ba5707
Nikita Krivosheev <nikita47598@mail.ru>
2023-02-13 14:28:12 +0300

    IntriArray

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -22,6 +22,14 @@
 
 //-------------------------------------------------------------------------------- 
 
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
 int a1 = 15;
 int b1 = 21;
 int c1 = 323564;
@@ -31,14 +39,6 @@ int c2 = 33;
 int a3 = 5632;
 int b3 = 23123;
 int c3 = 313;
-
-int Max(int arg1, int arg2, int arg3)
-{
-    int result = arg1;
-    if(arg2 > result) result = arg2;
-    if(arg3 > result) result = arg3;
-    return result;
-}
 
 // int max1 =  Max(a1, b1, c1);
 // int max2 =  Max(a2, b2, c2);
---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = {15, 21, 323564, 12, 2311, 33, 5632, 23123, 313};
+// array[0] = 12;
+// Console.WriteLine(array[4]);
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8]));
+
+Console.WriteLine(max);
