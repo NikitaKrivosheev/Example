b36c9c745a2ecf7cdb2d1756016fed32e6ae3d3d
Nikita Krivosheev <nikita47598@mail.ru>
2023-02-13 15:07:02 +0300

    MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,34 @@
+﻿// Console.WriteLine("Введите число:");
+// int[] array = { 17, 12, 21, 45, 34, 84, 92, 53, 75, 65, 21 };
+// int n = array.Length;
+// int index = 0;
+// int find = Convert.ToInt32(Console.ReadLine());
+
+// while (index < n)
+// {
+//     if (array[index] == find)
+//     {
+//         Console.Write($"index числа {find} = ");
+//         Console.WriteLine(index);
+//         break;                                       // break - (прервать) цикл после выполнение условия                              
+//     }
+//     index = index + 1;
+// }
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+int[] array = { 17, 12, 21, 45, 34, 84, 92, 53, 75, 65, 21 };
+int n = array.Length;
+int index = 0;
+int find = 21;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;                                       // break - (прервать) цикл после выполнение условия 
+    }
+    index = index + 1;
+}
