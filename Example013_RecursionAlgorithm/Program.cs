19d1908ba66bde9b03bc07a0c8f3ae7886ede8f4
Nikita Krivosheev <nikita47598@mail.ru>
2023-03-11 21:12:46 +0300

    example013-

--- Example013_RecursionAlgorithm/Program.cs
+++ Example013_RecursionAlgorithm/Program.cs
@@ -131,13 +131,321 @@
 // f(n) = f(n - 1) + f(n - 2) 
 
 // int
-double Fibonacci(int n)
-{
-    if (n == 1 || n == 2) return 1;
-    else return Fibonacci(n - 1) + Fibonacci(n - 2);
-}
+// double Fibonacci(int n)
+// {
+//     if (n == 1 || n == 2) return 1;
+//     else return Fibonacci(n - 1) + Fibonacci(n - 2);
+// }
 
-for (int i = 1; i < 50; i++)
-{
-    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
-}
+// for (int i = 1; i < 50; i++)
+// {
+//     Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+// }
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// string NumbersFor(int a, int b)
+// {
+//     string result = String.Empty;
+//     for (int i = a; i <= b; i++)
+//     {
+//         result += $"{i} ";
+//     }
+//     return result;
+// }
+
+// string NumbersRec(int a, int b)
+// {
+//     if (a <= b) return $"{a} " + NumbersRec(a + 1, b);
+//     else return String.Empty;
+// }
+
+// Console.WriteLine(NumbersFor(1, 10));
+// Console.WriteLine(NumbersRec(1, 10));
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// string NumbersFor(int a, int b)
+// {
+//     string result = String.Empty;
+//     for (int i = a; i >= b; i--)
+//     {
+//         result += $"{i} ";
+//     }
+//     return result;
+// }
+
+// string NumbersRec(int a, int b)
+// {
+//     if (a <= b) return NumbersRec(a + 1, b) + $"{a} ";
+//     else return String.Empty;
+// }
+
+// Console.WriteLine(NumbersFor(1, 10));
+// Console.WriteLine(NumbersRec(1, 10));
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// int SumFor(int n)
+// {
+//     int result = 0;
+//     for (int i = 1; i <= n; i++) result += i;
+//     return result;
+// }
+
+// int SumRec(int n)
+// {
+//     if (n == 0) return 0;
+//     else return n + SumRec(n - 1);
+// }
+
+// Console.WriteLine(SumFor(10));
+// Console.WriteLine(SumRec(10));
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// int FactorialFor(int n)
+// {
+//     int result = 1;
+//     for (int i = 1; i <= n; i++) result *= i;
+//     return result;
+// }
+
+// int FactorialRec(int n)
+// {
+//     if (n == 0) return 1;
+//     else return n * FactorialRec(n - 1);
+// }
+
+// Console.WriteLine(FactorialFor(10));
+// Console.WriteLine(FactorialRec(10));
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// int PowerFor(int a, int n)
+// {
+//     int result = 1;
+//     for (int i = 1; i <= n; i++) result *= a;
+//     return result;
+// }
+
+// int PowerRec(int a, int n)
+// {
+//     // return n == 0 ? 1 : PowerRec(a, n - 1) * a;
+//     if (n == 0) return 1;
+//     else return PowerRec(a, n - 1) * a;
+// }
+
+// int PowerRecMath(int a, int n)
+// {
+//     // return n == 0 ? 1 : PowerRec(a, n - 1) * a;
+//     if (n == 0) return 1;
+//     else if (n % 2 == 0) return PowerRecMath(a * a, n / 2);
+//     else return PowerRecMath(a, n - 1) * a;
+// }
+
+// Console.WriteLine(PowerFor(2, 10));
+// Console.WriteLine(PowerRec(2, 10));
+// Console.WriteLine(PowerRecMath(2, 10));
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// char[] s = { "а", "и", "с", "в" };
+// {
+//     int count = s.Length;
+//     int n = 1;
+//     for (int i = 0; i <= count; i++)
+//     {
+//         for (int j = 0; j <= count; j++)
+//         {
+//             for (int k = 0; k <= count; k++)
+//             {
+//                 for (int l = 0; l <= count; l++)
+//                 {
+//                     for (int m = 0; m <= count; m++)
+//                     {
+//                         Console.WriteLine($"{n++,-5}{s[i]}{s[j]}{s[k]}{s[l]}");
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+
+
+// void FindWorlds(string alphabet, char[] word, int length = 0)
+// {
+//     int n = 0;
+//     if (length == word.Length)
+//     {
+//         Console.WriteLine($"{n++}{new String(word)}");
+//     }
+//     for (int i = 0; i <= alphabet.Length; i++)
+//     {
+//         word[length] = alphabet[i];
+//         FindWorlds(alphabet, word, length + 1);
+//     }
+// }
+
+// FindWorlds("аисв", new char[2]);
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// void CatalogInfo(string path, string indent = "")
+// {
+//     DirectoryInfo[] catalog = new DirectoryInfo(path);
+
+//     DirectoryInfo[] catalogs = catalog.GetDirectories();
+//     for (int i = 0; i < catalogs.Length; i++)
+//     {
+//         Console.WriteLine($"{indent}{catalogs[i].Name}");
+//         CatalogInfo(catalogs[i].FullName, indent + "  ");
+//     }
+
+//     FileInfo[] files = catalog.GetFiles();
+
+//     for (int i = 0; i < files.Length; i++)
+//     {
+//         Console.WriteLine($"{indent}{files[i].Name}");
+
+//     }
+// }
+
+// string path = @"Example001_HelloConsole";
+// CatalogInfo(path);
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// void Towers(string with = "1", string on = "3", string some = "2", int count = 5)
+// {
+//     if (count > 1) Towers(with, some, on, count - 1);
+//     Console.WriteLine($"{with} >> {on}");
+//     if (count > 1) Towers(some, on, with, count - 1);
+// }
+
+// Towers();
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// string emp = String.Empty;
+// string[] tree = { emp, "/", "*", "10", "-", "+", emp, emp, "4", "2", "1", "3" };
+// //                 0    1    2     3    4    5    6    7    8    9    10   11
+// void InOrderTraversal(int pos = 1)
+// {
+//     if (pos < tree.Length)
+//     {
+//         int left = 2 * pos;
+//         int right = 2 * pos + 1;
+//         if (left < tree.Length && !String.IsNullOrEmpty(tree[left])) InOrderTraversal(left);
+//         Console.WriteLine(tree[pos]);
+//         if (right < tree.Length && !String.IsNullOrEmpty(tree[right])) InOrderTraversal(right);
+//     }
+// }
+
+// InOrderTraversal();
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// decimal fRec = 0;
+// decimal fIte = 0;
+
+// decimal FibonacciRecursion(int n)
+// {
+//     fRec++;
+//     return n == 0 || n == 1 ? 1 : FibonacciRecursion(n - 1) + FibonacciRecursion(n - 2);
+// }
+
+// decimal FibonacciIteration(int n)
+// {
+//     fIte++;
+//     decimal result = 1;
+//     decimal f0 = 1;
+//     decimal f1 = 1;
+//     for (int i = 2; i < n; i++)
+//     {
+//         result = f0 + f1;
+
+//         f0 = f1;
+//         f1 = result;
+//         fIte++;
+//     }
+//     return result;
+// }
+
+// //# 1 2 3 4 5 6
+// //V 1 1 2 3 5 8
+
+// Console.ReadLine();
+
+// DateTime dt = DateTime.Now;
+// for (int n = 10; n < 40; n++)
+// {
+//     Console.WriteLine($"FibonacciIteration({n}) = {FibonacciIteration(n)} fIte = {fIte.ToString("### ### ###"),-15}");
+//     fIte = 0;
+// }
+// Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
+
+// Console.WriteLine();
+// Console.ReadLine();
+// dt = DateTime.Now;
+// for (int n = 10; n < 40; n++)
+// {
+//     Console.WriteLine($"FibonacciRecursion({n}) = {FibonacciRecursion(n)} fRec = {fRec.ToString("### ### ###"),-15}");
+//     fRec = 0;
+// }
+// Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//переполнение
+
+// int i = 0;
+// void Rec()
+// {
+//     Rec();
+//     Console.WriteLine(i++);
+// }
+
+// Rec();
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//переполнение
+
+// int[,] pic = new int[1000, 1000];
+// for (int i = 0; i < 1000; i++)
+// {
+//     pic[0, i] = 1;
+//     pic[i, 0] = 1;
+//     pic[i, 999] = 1;
+//     pic[999, i] = 1;
+// }
+
+// void PrintImage(int[,] image)
+// {
+//     for (int i = 0; i < image.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < image.GetLength(1); j++)
+//         {
+//             // Console.Write($"{ image[i, j]} ");
+//             if (image[i, j] == 0) Console.Write($" ");
+//             else Console.Write($"+");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void FillImage(int row, int col)
+// {
+//     if (pic[row, col] == 0)
+//     {
+//         pic[row, col] = 1;
+//         FillImage(row - 1, col);
+//         FillImage(row, col - 1);
+//         FillImage(row + 1, col);
+//         FillImage(row, col + 1);
+//     }
+// }
+
+// PrintImage(pic);
+// FillImage(1, 1);
+// Console.WriteLine();
+// PrintImage(pic);
